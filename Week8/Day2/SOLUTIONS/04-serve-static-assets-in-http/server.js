9bfec8499d8d02bc8df9dab514074370c3132cec
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-01-18 14:18:22 -0800

    serving static assets

--- Week8/Day2/SOLUTIONS/04-serve-static-assets-in-http/server.js
+++ Week8/Day2/SOLUTIONS/04-serve-static-assets-in-http/server.js
@@ -1,10 +1,56 @@
-const http = require('http');
+const http = require("http");
 const fs = require("fs");
+
+const getContentType = (extension) => {
+  // switch (extension) {
+  //   case "css":
+  //     return "text/css";
+  //   case "jpg" || "jpeg":
+  //     return "image/jpeg";
+  //   default:
+  //     return "text/plain";
+  // }
+
+  if (extension === "css") {
+    return "text/css";
+  } else if (extension === "jpg" || extension === "jpeg") {
+    return "image/jpeg";
+  } else {
+    return "text/plain";
+  }
+};
 
 const server = http.createServer((req, res) => {
   // Your code here
+  if (req.method === "GET" && req.url.startsWith("/static")) {
+    const urlParts = req.url.split("/static");
+    console.log("url parts ", urlParts);
+
+    const assetPath = urlParts[1];
+    console.log("asset path ", assetPath);
+
+    const resBody = fs.readFileSync("./assets" + assetPath); // ./assets/css/application.css
+
+    const extension = assetPath.split(".")[1];
+    console.log("ext ", extension);
+
+    res.statusCode = 200;
+
+    const contentType = getContentType(extension);
+    res.setHeader("Content-Type", contentType);
+
+    return res.end(resBody);
+  }
+
+  if (req.method === "GET" && req.url === "/") {
+    const resBody = fs.readFileSync("index.html", "utf-8");
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    return res.end(resBody);
+  }
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
