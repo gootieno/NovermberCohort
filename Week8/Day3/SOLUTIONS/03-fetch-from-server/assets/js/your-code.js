03d638c8ad5756c5b11f50c2bf12f9b3a8d72012
Tanner Shaw <traumaaa@gmail.com>
2023-01-19 12:55:52 -0600

    fetch from server practice solutions

--- Week8/Day3/SOLUTIONS/03-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/SOLUTIONS/03-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,35 @@
 export function getAllDogs() {
     // Your code here
+    return fetch('/dogs')
 }
 
 export function getDogNumberTwo() {
     // Your code here
+    return fetch('/dogs/2')
 }
 
 export function postNewDog() {
     // Your code here
+    return fetch('/dogs', {
+        method: 'POST',
+        headers: { 'Content-Type': 'x-www-form-urlencoded' },
+        body: new URLSearchParams({ name: 'Dag', age: 10 })
+    })
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+    // Your code here
+    return fetch('/dogs', {
+        method: 'POST',
+        headers: { 'Content-Type': 'x-www-form-urlencoded' },
+        body: new URLSearchParams({ name: name, age: age })
+    })
 }
 
 export function deleteDog(id) {
-      // Your code here
+    // Your code here
+    return fetch(`/dogs/${id}/delete`, {
+        method: 'POST',
+        headers: { 'AUTH': 'ckyut5wau0000jyv5bsrud90y' }
+    })
 }
