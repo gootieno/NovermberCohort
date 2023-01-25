6cddd6efa1359072b9392a5f20af169c52c7b2c7
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-01-25 11:21:23 -0800

    storing data on elements

--- Week9/Day3/SOLUTIONS/02-data-on-elements/data.js
+++ Week9/Day3/SOLUTIONS/02-data-on-elements/data.js
@@ -1,1 +1,24 @@
 // Your code here
+const buildShoppingList = () => {
+  const shoppingListAdd = document.querySelector("#add");
+  //   console.log("form element ", shoppingListAdd);
+  shoppingListAdd.addEventListener("click", (event) => {
+    event.preventDefault();
+    // console.log("event ", event);
+    const input = document.getElementById("name");
+    console.log("input ", input);
+    const shoppingListContainer = document.getElementById("shopping-list");
+
+    const type = document.querySelector("#type").value;
+
+    const listItem = document.createElement("li");
+    listItem.innerText = input.value;
+    listItem.setAttribute("data-type", type);
+
+    shoppingListContainer.append(listItem);
+
+    input.value = "";
+  });
+};
+
+window.onload = buildShoppingList;
